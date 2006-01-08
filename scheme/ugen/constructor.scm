bc6b4c00a6a9f614d17152b9c87169b57e849730
Rohan Drape <rd@slavepianos.org>
2006-01-07 18:17:23 -0800

    Simplify UGen constructors
    darcs-hash:2bf9ed6c069c37234d195900b595b3a7005d5ec2

--- scheme/ugen/constructor.scm
+++ scheme/ugen/constructor.scm
@@ -1,17 +1,31 @@
 ;; constructor.scm - (c) rohan drape, 2005
 
+;; name = <string>
+;; rate? = <integer> | #f
+;; inputs = <list> of <input*>
+;; mce? = <input*> | #f
+;; outputs = <integer>
+;; special = <integer>
+
+(define (construct-ugen name rate? inputs mce? outputs special) 
+  (let ((rate (if rate?
+		  rate?
+		  (maximum (map rate-of inputs))))
+	(inputs* (if mce?
+		     (++ inputs (mce-force mce?))
+		     inputs)))
+    (make-ugen/proxies
+     (symbol->string name)
+     rate
+     inputs*
+     (make-outputs outputs rate)
+     special)))
+
 (define-syntax define-oscillator
   (syntax-rules ()
     ((_ n (i ...) o) 
      (define (n r i ...) 
-       (make-ugen/proxies
-	(symbol->string (quote n))
-	r
-	(list i ...) 
-	(make-outputs o r)
-	0)))))
-
-;; Number of outputs is zth input...
+       (construct-ugen (quote n) r (list i ...) #f o 0)))))
 
 (define (without n l)
   (append (take l n) (drop l (+ n 1))))
@@ -19,105 +33,54 @@
 (define-syntax define-oscillator-n
   (syntax-rules ()
     ((_ n (i ...) z) 
-     (define (n r i ...)
+     (define (n rate i ...)
        (let ((l (list i ...)))
-	 (make-ugen/proxies
-	  (symbol->string (quote n))
-	  r
-	  (without z l)
-	  (make-outputs (ref l z) r)
-	  0))))))
+	 (construct-ugen 'n rate (without z l) #f (ref l z) 0))))))
 
 (define-syntax define-oscillator*
   (syntax-rules ()
     ((_ n (i ... v) o)
      (define (n r i ... v) 
-       (make-ugen/proxies
-	(symbol->string (quote n))
-	r
-	(append (list i ...) (mce-force v))
-	(make-outputs o r)
-	0)))))
+       (construct-ugen 'n r (list i ...) v o 0)))))
 
 (define-syntax define-oscillator!
   (syntax-rules ()
     ((_ n (i ...) o)
      (define (n id r i ...) 
-       (make-ugen/proxies
-	(symbol->string (quote n))
-	r
-	(list i ...)
-	(make-outputs o r)
-	0)))))
+       (construct-ugen 'n r (list i ...) #f o 0)))))
 
 (define-syntax define-filter
   (syntax-rules ()
     ((_ n (i ...) o) 
      (define (n i ...)
-       (let ((r (maximum (map rate-of (list i ...)))))
-	 (make-ugen/proxies 
-	  (symbol->string (quote n))
-	  r
-	  (list i ...) 
-	  (make-outputs o r)
-	  0))))))
+       (construct-ugen 'n #f (list i ...) #f o 0)))))
 
 (define-syntax define-filter*
   (syntax-rules ()
     ((_ n (i ... v) o) 
      (define (n i ... v)
-       (let* ((i* (append (list i ...) (mce-force v)))
-	      (r (maximum (map rate-of i*))))
-	 (make-ugen/proxies
-	  (symbol->string (quote n))
-	  r
-	  i*
-	  (make-outputs o r)
-	  0))))))
+       (construct-ugen 'n #f (list i ...) v o 0)))))
 
 (define-syntax define-filter!
   (syntax-rules ()
     ((_ n (i ...) o) 
      (define (n id i ...)
-       (let ((r (maximum (map rate-of (list i ...)))))
-	 (make-ugen/proxies
-	  (symbol->string (quote n))
-	  r
-	  (list i ...) 
-	  (make-outputs o r)
-	  0))))))
+       (construct-ugen 'n #f (list i ...) #f o 0)))))
 
 (define-syntax define-unary-operator
   (syntax-rules ()
     ((_ n s) 
      (define (n a)
-       (let ((r (rate-of a)))
-	 (make-ugen/proxies
-	  "UnaryOpUGen"
-	  r
-	  (list a)
-	  (make-outputs 1 r)
-	  s))))))
+       (construct-ugen 'UnaryOpUGen #f (list a) #f 1 s)))))
 
 (define-syntax define-binary-operator
   (syntax-rules ()
     ((_ n s) 
      (define (n a b)
-       (let ((r (max (rate-of a) (rate-of b))))
-	 (make-ugen/proxies
-	  "BinaryOpUGen"
-	  r 
-	  (list a b) 
-	  (make-outputs 1 r)
-	  s))))))
+       (construct-ugen 'BinaryOpUGen #f (list a b) #f 1 s)))))
 
 (define-syntax define-specialized
   (syntax-rules ()
     ((_ n (i ...) o r)
      (define (n i ...)
-       (make-ugen/proxies
-	(symbol->string (quote n))
-	r
-	(list i ...) 
-	(make-outputs o r)
-	0)))))
+       (construct-ugen 'n r (list i ...) #f o 0)))))
