c4b7a453b2f7e62756338a6bf8ea4d66ff7d2aed
Rohan Drape <rd@slavepianos.org>
2006-05-06 17:03:22 -0700

    Stepper help file edits
    darcs-hash:afdd61ec67df29897457421eca45bb3ffa6411d8

--- help/UGens/Triggers/Stepper.help.scm
+++ help/UGens/Triggers/Stepper.help.scm
@@ -11,7 +11,14 @@
 	       155.563)))
   (-> s (/b_setn* 10 0 a)))
 
-(require (lib "1.ss" "srfi"))
+;; Function composition...
+
+(define (seq s l)
+  (if (null? l)
+      s
+      (seq ((car l) s) (cdr l))))
+
+(require (only (lib "1.ss" "srfi") make-list))
 
 (let* ((rate (MouseX kr 1 5 1 0.1))
        (clock (Impulse kr rate 0))
@@ -22,27 +29,25 @@
 		       (Lag2 freq 0.1)
 		       (Mul (MouseY kr 80 1600 1 0.1) (Add (Mul env 4) 2)))
 		   (Mce 0 0.3)))
-       (lfo (SinOsc kr 0.2 (Mce 0 half-pi 0.0024 0.0025))))
-  (let* ((s (mix (LFPulse ar (Mul freq (Mce 1 3/2 2)) 0 0.3)))
-	 (s (Mul (RLPF s ffreq 0.3) env))
-	 (s (Mul (RLPF s ffreq 0.3) env))
-	 (s (Mul s 0.2))
-	 ;; Echo
-	 (s (MulAdd (CombL s 1 (FDiv 0.66 rate) 2) 0.8 s))
-	 ;; Reverb
-	 (s (let ((rev s))
-	       (for-each (lambda (_)
-			   (set! rev (AllpassN rev
-					       0.05
-					       (make-mce (randl 2 0 0.05))
-					       (rand 1.5 2.0))))
-			 (iota 5))
-	       (Add s (Mul rev 0.3))))
-	 (s (LeakDC s 0.1))
-	 ;; Flanger
-	 (s (Add (DelayL s 0.1 lfo) s)))
-    ;; Slight bass emphasis
-    (OnePole s 0.9)))
+       (lfo (SinOsc kr 0.2 (Mce 0 half-pi 0.0024 0.0025)))
+       (rvb (lambda (s) (AllpassN s
+				  0.05
+				  (make-mce (randl 2 0 0.05))
+				  (rand 1.5 2.0)))))
+  (seq (mix (LFPulse ar (Mul freq (Mce 1 3/2 2)) 0 0.3))
+       (list 
+	(lambda (s) (Mul (RLPF s ffreq 0.3) env))
+	(lambda (s) (Mul (RLPF s ffreq 0.3) env))
+	(lambda (s) (Mul s 0.2))
+	;; Echo
+	(lambda (s) (MulAdd (CombL s 1 (FDiv 0.66 rate) 2) 0.8 s))
+	;; Reverb
+	(lambda (s) (Add s (Mul (seq s (make-list 5 rvb)) 0.3)))
+	(lambda (s) (LeakDC s 0.1))
+	;; Flanger
+	(lambda (s) (Add (DelayL s 0.1 lfo) s))
+	;; Slight bass emphasis
+	(lambda (s) (OnePole s 0.9)))))
 
 ;; Pattern randomizer....
 
@@ -50,3 +55,32 @@
 		(midicps (+ 36 (degree->key e (list 0 3 5 7 10) 12))))
 	      (map floor-exact (randl 16 0 15)))))
   (-> s (/b_setn* 10 0 p)))
+
+;; A shorter variant, using some simple syntax...
+
+(define-syntax seq*
+  (syntax-rules ()
+    ((_ i s f ...)
+     (seq i (list (lambda (s) f) ...)))))
+
+(let* ((rate (MouseX kr 1 5 1 0.1))
+       (clock (Impulse kr rate 0))
+       (env (Decay2 clock 0.002 2.5))
+       (index (Stepper clock 0 0 15 1 0))
+       (freq (BufRd kr 1 10 index 1 1))
+       (ffreq (Add (Lag2 freq 0.1) (Mce 0 0.3)))
+       (lfo (SinOsc kr 0.2 (Mce 0 half-pi 0.0024 0.0025)))
+       (rvb (lambda (s) (AllpassN s
+				  0.05
+				  (make-mce (randl 2 0 0.05))
+				  (rand 1.5 2.0)))))
+  (seq* (mix (LFPulse ar (Mul freq (Mce 1 3/2 2)) 0 0.3))
+	s
+	(Mul (RLPF s ffreq 0.3) env)
+	(Mul (RLPF s ffreq 0.3) env)
+	(Mul s 0.2)
+	(MulAdd (CombL s 1 (FDiv 0.66 rate) 2) 0.8 s)
+	(Add s (Mul (seq s (make-list 5 rvb)) 0.3))
+	(LeakDC s 0.1)
+	(Add (DelayL s 0.1 lfo) s)
+	(OnePole s 0.9)))
