75bbc16b94ee333ff2e96aba4617c7040363f668
Rohan Drape <rd@slavepianos.org>
2006-05-07 23:40:38 -0700

    Add wchoose and pchoose
    darcs-hash:f93e3e68f96883d1b1c90bba42f66f8c109ee5a1

--- scheme/random/choose.scm
+++ scheme/random/choose.scm
@@ -5,8 +5,26 @@
 (define (choose l)
   (ref l (randi 0 (length l))))
 
+;; Weighted choose, w must sum to 1.
+
+(define (windex w n)
+  (index (lambda (e) (< n e)) (cdr (dx->d 0 w))))
+
+(define (wchoose l w)
+  (ref l (windex w (rand 0.0 1.0))))
+
+;; Proportional choose, p will be normalized and then wchoosen.
+
+(define (normalize-sum l)
+  (let ((n (foldl1 + l)))
+    (map (lambda (e) (/ e n)) l)))
+
+(define (pchoose l p)
+  (wchoose l (normalize-sum p)))
+
 ;; Evaluates to a random element from the proper list `l' as if it
 ;; were a list distributed with the proportions at `proportions'.
+;; This is pchoose with reversed arguments...
 
 (define (choose/proportions proportions l)
   (let* ((p-sum (cdr (dx->d 0 proportions)))
