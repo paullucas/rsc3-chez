45d2ef4398682c2a05767de4c82f4c4cb56c0513
Rohan Drape <rd@slavepianos.org>
2006-09-11 03:52:44 -0700

    Delete extraneous begin
    darcs-hash:334cc2c60a6a171a5c2574a91d2eaf58b2c79db7

--- scheme/supercollider/play.scm
+++ scheme/supercollider/play.scm
@@ -6,6 +6,5 @@
   (let ((g (if (graphdef? u)
 	       u
 	       (ugen->graphdef/out u))))
-    (begin
-      (->< s (/d_recv (graphdef->u8l g)))
-      (->  s (/s_new (graphdef-name g) -1 1 1)))))
+    (->< s (/d_recv (graphdef->u8l g)))
+    (->  s (/s_new (graphdef-name g) -1 1 1))))
