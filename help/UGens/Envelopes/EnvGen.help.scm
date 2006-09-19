a50625f640095dc2f7aed047936e8024f0a82f6c
Rohan Drape <rd@slavepianos.org>
2006-09-19 06:46:33 -0700

    Additions to EnvGen help file
    darcs-hash:6b0127b4c9836a0d17fc11ecfc03acd00ca0ecc5

--- help/UGens/Envelopes/EnvGen.help.scm
+++ help/UGens/Envelopes/EnvGen.help.scm
@@ -4,11 +4,37 @@
 ;; reorders the inputs to this UGen so that the envelope is the first
 ;; argument.
 
-(Mul (EnvGen kr 1 0.1 0 1 2 (env/perc 0.01 1 1 -4))
+;; There are utilities for contructing the envelope argument.
+
+;; The arguments for levelScale, levelBias, and timeScale are polled
+;; when the EnvGen is triggered and remain constant for the duration
+;; of the envelope.
+
+;; envelope - an breakpoint set
+
+;; gate - this triggers the envelope and holds it open while > 0. If
+;;        the Env is fixed-length (e.g. Env.linen, Env.perc), the gate
+;;        argument is used as a simple trigger. If it is an sustaining
+;;        envelope (e.g. Env.adsr, Env.asr), the envelope is held open
+;;        until the gate becomes 0, at which point is released.
+
+;; levelScale - scales the levels of the breakpoints.
+
+;; levelBias - offsets the levels of the breakpoints.
+
+;; timeScale - scales the durations of the segments.
+
+;; doneAction - an integer representing an action to be executed when
+;;              the env is finished playing. This can be used to free
+;;              the enclosing synth, etc.
+
+;; Percussive envelope
+
+(Mul (EnvGen kr 1 0.1 0 1 removeSynth (env/perc 0.01 1 1 -4))
      (SinOsc ar 440 0))
 
 ;; The break-point assistant makes a static envelope from a
 ;; co-ordinate list.  There is a duration and amplitude scalar.
 
-(Mul (EnvGen kr 1 0.1 0 1 2 (env/bp '(0 0 1/2 1 1 0) 1 1))
+(Mul (EnvGen kr 1 0.1 0 1 removeSynth (env/bp '(0 0 1/2 1 1 0) 1 1))
      (SinOsc ar 440 0))
