d440c68d569466fa325268b83107c7330cd81326
Rohan Drape <rd@slavepianos.org>
2006-10-01 01:39:40 -0700

    Add help files for TChoose and TWChoose
    darcs-hash:758672d822451bbb726e101de8308e3a18610b2f

---
+++ help/UGens/Oscillators/TWChoose.help.scm
@@ -0,0 +1,21 @@
+;; (TWChoose trig array weights normalize)
+
+;; The output is selected randomly on recieving a trigger from an
+;; array of inputs.  The weights of this choice are determined from
+;; the weights array.  If normalize is set to 1 the weights are
+;; continuously normalized, which means an extra calculation overhead.
+;; When using fixed values the normalizeSum method can be used to
+;; normalize the values.  TWChoose is a composite of TWindex and
+;; Select
+
+(let ((a (Mce (SinOsc ar 220 0)
+	      (Saw ar 440)
+	      (Pulse ar 110 0.1))))
+  (Mul (TWChoose (Dust ar (MouseX kr 1 1000 1 0.1))
+		 a
+		 (Mce 0.99 0.05 0.05)
+		 1)
+       0.1))
+
+;; Note: all the ugens are continously running. This may not be the
+;; most efficient way if each input is cpu-expensive.
