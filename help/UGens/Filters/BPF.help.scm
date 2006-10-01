ac4cfc7760690e17f6a6842feeb44f644d4624df
Rohan Drape <rd@slavepianos.org>
2006-10-01 01:35:54 -0700

    Fix BPF help file
    darcs-hash:b7b79161afc5a63844564aaa8c5c6196554dbe20

--- help/UGens/Filters/BPF.help.scm
+++ help/UGens/Filters/BPF.help.scm
@@ -6,7 +6,11 @@
 ;; freq  - cutoff frequency in Hertz.
 ;; rq    - the reciprocal of Q.  bandwidth / cutoffFreq. 
 
-(BPF ar (Saw ar 200 0.5) (FSinOsc kr (XLine kr 0.7 300 20) 0 3600 4000) 0.3)
+(BPF (Mul (Saw ar 200) 0.5) 
+     (MulAdd (FSinOsc kr (XLine kr 0.7 300 20 removeSynth) 0) 3600 4000) 
+     0.3)
 
-(let ((f (MouseX kr 220 440))) 
-  (BPF ar (WhiteNoise ar) (list f (-u 550 f)) (MouseY kr 0 0.01)))
+(let ((f (MouseX kr 220 440 0 0.1))) 
+  (BPF (WhiteNoise ar)
+       (Mce f (Sub 550 f))
+       (MouseY kr 0 0.01 0 0.1)))
