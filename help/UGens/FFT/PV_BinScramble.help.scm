46e5437ccbc566ad75279516b4a973c680048087
Rohan Drape <rd@slavepianos.org>
2006-10-01 01:35:25 -0700

    Fix PV_BinScramble help file
    darcs-hash:aa4fc1fc16f8dcd1a1c7d52e2c564a4ea1940470

--- help/UGens/FFT/PV_BinScramble.help.scm
+++ help/UGens/FFT/PV_BinScramble.help.scm
@@ -12,15 +12,15 @@
 
 ;; trig - a trigger selects a new random ordering.
 
-(begin 
+(begin
   (->< s (/b_alloc 10 2048 1))
-  (->< s (/b_allocRead 12 (rsc-file "audio/metal.wav") 0 0)))
+  (->< s (/b_allocRead 12 "/home/rohan/sw/sw-01/audio/metal.wav" 0 0)))
 
 (let* ((a (PlayBuf ar 1 12 (BufRateScale kr 12) 1 0 1))
        (f (FFT 10 a))
        (g (PV_BinScramble f
-			  (MouseX kr 0.0 1.0 0 0.1) 
-			  0.1
-			  (GT (MouseY kr 0.0 1.0 0 0.1) 0.5)))
+			  (MouseX kr 0.0 1.0 0 0.1)
+			  (MouseY kr 0.0 1.0 0 0.1)
+			  (Impulse kr 4 0)))
        (h (IFFT g)))
   (Out 0 (Mul 0.5 (Mce h h))))
