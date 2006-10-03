4e3d554124adcaac633ede8fad101c17412cb39c
Rohan Drape <rd@slavepianos.org>
2006-10-02 19:36:02 -0700

    Translate PV_ConformalMap example from sclang help file
    darcs-hash:7d10426ca8952cc0b6c65270a6e69a3dcbd61f51

--- help/UGens/FFT/PV_ConformalMap.help.scm
+++ help/UGens/FFT/PV_ConformalMap.help.scm
@@ -13,10 +13,30 @@
 
 (->< s (/b_alloc 10 1024 1))
 
-(Pan2 
- (IFFT 
-  (PV_ConformalMap 
-   (FFT 10 (Mul (In ar (NumInputBuses ir) 1) 0.5)) (MouseX kr -1 1 0 0.1) 
+(Pan2
+ (IFFT
+  (PV_ConformalMap
+   (FFT 10 (Mul (In ar (NumInputBuses ir) 1) 0.5)) (MouseX kr -1 1 0 0.1)
    (MouseY kr -1 1 0 0.1)))
  0
  1)
+
+(define (signal n)
+  (let ((f (MulAdd (SinOsc kr (make-mce (randl n 0.1 0.5)) 0)
+		   10
+		   (Mul (Mce 1 1.1 1.5 1.78 2.45 6.7) 220))))
+    (mix (Mul (LFSaw ar f 0) 0.3))))
+
+(define (mapped n)
+  (let* ((c0 (FFT 0 (signal n)))
+	 (c1 (PV_ConformalMap c0
+			      (MouseX kr 0.01 2.0  1.0 0.1)
+			      (MouseY kr 0.01 10.0 1.0 0.1))))
+    (IFFT c1)))
+
+(mapped 3)
+
+(let ((s (mapped 3)))
+  (Out 0 (Pan2 (MulAdd (CombN s 0.1 0.1 10) 0.5 s)
+	       0
+	       1)))
