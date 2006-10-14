d673dedc18c02629d4af473b9404b66b09e4a91a
Rohan Drape <rd@slavepianos.org>
2006-10-14 04:19:34 -0700

    Fixes to DegreeToKey help file
    darcs-hash:7ba2c4f0aa06774365f5ebd7adfefb3b9f3d99f4

--- help/UGens/Controls/DegreeToKey.help.scm
+++ help/UGens/Controls/DegreeToKey.help.scm
@@ -18,7 +18,7 @@
 (-> s (/b_setn* 0 0 (list 0 2 3.2 5 7 9 10)))
 
 (let* ((k (DegreeToKey 0 (MouseX kr 0 15 0 0.1) 12))
-       (n (Mul (LFNoise1 kr (Mce 3 3)) 0.04))
+       (n (Mul (LFNoise1 kr (Mce 3 3.05)) 0.04))
        (f (lambda (b)
 	    (let* ((o (Mul (SinOsc ar (MIDICPS (Add* b k n)) 0) 0.1))
 		   (d (Mul (RLPF (LFPulse ar (MIDICPS (Mce 48 55)) 0.15 0.5)
@@ -27,4 +27,4 @@
 			   0.1))
 		   (m (Add o d)))
 	      (Add (CombN m 0.31 0.31 2) m)))))
-  (Mul (Add (f 48) (f 72)) 0.5))
+  (Mul (Add (f 48) (f 72)) 0.25))
