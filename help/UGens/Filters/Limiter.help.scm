2088eb62a7a351775b2f93015f820d50bb1685f7
Rohan Drape <rd@slavepianos.org>
2006-10-28 04:14:05 -0700

    Fix example in Limiter help file
    darcs-hash:f4fc276c4aeb5a96547e7c180e72bed93f129eca

--- help/UGens/Filters/Limiter.help.scm
+++ help/UGens/Filters/Limiter.help.scm
@@ -5,12 +5,10 @@
 ;; to look ahead in the audio. Thus there is a delay equal to twice
 ;; the lookAheadTime.  Limiter, unlike Compander, is completely
 ;; transparent for an in range signal.
- 
-(Limiter ar (Decay2 ar (Impulse ar 8 (LFSaw kr 0.25 -0.6 0.7))
-		       0.001 0.3 (FSinOsc ar 500))
-	    0.4 0.01)
 
-(let ((z (Decay2 ar (Impulse ar 8 (LFSaw kr 0.25 -0.6 0.7))
-		    0.001 0.3 (FSinOsc ar 500))))
-  (list z (Limiter ar z 0.4 0.01)))
-
+(Limiter (Mul (Decay2 (Impulse ar 8 (Mul (LFSaw kr 0.25 -0.6) 0.7))
+		      0.001 
+		      0.3)
+	      (FSinOsc ar 500 0))
+	 0.4 
+	 0.01)
