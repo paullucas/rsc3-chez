2431605d269d194792901923e4930c11b3244b4f
Rohan Drape <rd@slavepianos.org>
2006-10-29 02:14:46 -0800

    Fixes to LFTri help file
    darcs-hash:6ea7811135ccab03dd352f1268524d7542df418f

--- help/UGens/Oscillators/LFTri.help.scm
+++ help/UGens/Oscillators/LFTri.help.scm
@@ -3,9 +3,8 @@
 ;; A non-band-limited triangular waveform oscillator. Output ranges
 ;; from -1 to +1.
 
-(LFTri ar 500 1 0.1)
+(Mul (LFTri ar 500 1) 0.1)
 
 ;; Used as both Oscillator and LFO.
 
-(LFTri ar (LFTri kr 4 0 400 400) 0 0.1)
-
+(Mul (LFTri ar (MulAdd (LFTri kr 4 0) 400 400) 0) 0.1)
