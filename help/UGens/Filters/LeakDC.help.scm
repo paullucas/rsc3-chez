bfa576af755fb63315fc775aa9a9b2c50b9c0038
Rohan Drape <rd@slavepianos.org>
2006-11-01 03:33:17 -0800

    Minor help file edits
    darcs-hash:253a3838c0393420dc260a02615e17f86c5d7a81

--- help/UGens/Filters/LeakDC.help.scm
+++ help/UGens/Filters/LeakDC.help.scm
@@ -1,7 +1,9 @@
 ;; (LeakDC in coef)                
 
-;; Remove DC.  This filter removes a DC offset from a signal.  in -
-;; input signal.  coef - leak coefficient.
+;; Remove DC.  This filter removes a DC offset from a signal.
+
+;; in   - input signal
+;; coef - leak coefficient
 
 (let ((a (Mul (LFPulse ar 800 0.5 0.5) 0.1)))
   (Mce a (LeakDC a 0.995)))
