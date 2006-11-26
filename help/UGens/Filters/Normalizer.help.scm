53597064025b608a93b9c94da82115500a7eddc9
Rohan Drape <rd@slavepianos.org>
2006-11-25 18:37:46 -0800

    Edit Normalizer help file
    darcs-hash:cb2f41d66899602e5472d5b1bc0f6d366cb2312d

--- help/UGens/Filters/Normalizer.help.scm
+++ help/UGens/Filters/Normalizer.help.scm
@@ -2,6 +2,8 @@
 
 ;; Flattens dynamics.
 
-(let ((z (Decay2 ar (Impulse ar 8 (LFSaw kr 0.25  -0.6  0.7))
-		    0.001  0.3 (FSinOsc ar 500))))
-  (list z (Normalizer ar z 0.4 0.01)))
+(let ((z (Mul (Decay2 (Impulse ar 8 (Mul (LFSaw kr 0.25 -0.6) 0.7))
+		      0.001  
+		      0.3)
+	      (FSinOsc ar 500 0))))
+  (Mce z (Normalizer z 0.4 0.01)))
