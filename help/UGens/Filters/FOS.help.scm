c52efc50e8b53cd73a7c73f6d39714e7ee70fcc3
Rohan Drape <rd@slavepianos.org>
2006-11-25 18:37:32 -0800

    Edit FOS help file
    darcs-hash:9aa1e1782e23ef5c892271badede8227751615cd

--- help/UGens/Filters/FOS.help.scm
+++ help/UGens/Filters/FOS.help.scm
@@ -4,10 +4,10 @@
 
 ;; Same as OnePole.
 
-(let ((x (LFTri ar 0.4 0 0.99)))
-  (FOS ar (LFSaw ar 200 0 0.2) (Sub 1 (Abs x)) 0 x))
+(let ((x (Mul (LFTri ar 0.4 0) 0.99)))
+  (FOS (Mul (LFSaw ar 200 0) 0.2) (Sub 1 (Abs x)) 0 x))
 
 ;; Same as OneZero
 
-(let ((x (LFTri ar 0.4 0 0.99)))
-  (FOS ar (LFSaw ar 200 0 0.2) (Sub 1 (Abs x)) x 0))
+(let ((x (Mul (LFTri ar 0.4 0) 0.99)))
+  (FOS (Mul (LFSaw ar 200 0) 0.2) (Sub 1 (Abs x)) x 0))
