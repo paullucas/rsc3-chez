f4bff048ddfb882cbe148f001bb4ad07f6fe6ceb
Rohan Drape <rd@slavepianos.org>
2006-12-01 17:56:22 -0800

    Edits to LFCub help file
    darcs-hash:e28813da8bae5d2731aaa9d122178c1a223ae96e

--- help/UGens/Oscillators/LFCub.help.scm
+++ help/UGens/Oscillators/LFCub.help.scm
@@ -2,24 +2,28 @@
  
 ;; A sine like shape made of two cubic pieces. Smoother than LFPar.
 
-(LFCub ar (LFCub kr (LFCub kr 0.2 0 8 10) 0 400 800) 0 0.1)
-(LFCub ar (LFCub kr 0.2 0 400 800) 0 0.1)
-(LFCub ar 800 0 0.1)
-(LFCub ar (XLine kr 100 8000 30) 0 0.1)
+(Mul (LFCub ar (MulAdd (LFCub kr (MulAdd (LFCub kr 0.2 0) 8 10) 0) 400 800) 0) 0.1)
+(Mul (LFCub ar (MulAdd (LFCub kr 0.2 0) 400 800) 0) 0.1)
+(Mul (LFCub ar 800 0) 0.1)
+(Mul (LFCub ar (XLine kr 100 8000 30 doNothing) 0) 0.1)
 
 ;; Compare:
 
-(LFPar ar (LFPar kr (LFPar kr 0.2 0 8 10) 0 400 800) 0 0.1)
-(LFPar ar (LFPar kr 0.2 0 400 800) 0 0.1)
-(LFPar ar 800 0 0.1)
-(LFPar ar (XLine kr 100 8000 30) 0 0.1)
+(Mul (LFPar ar (MulAdd (LFPar kr (MulAdd (LFPar kr 0.2 0) 8 10) 0) 400 800) 0) 0.1)
+(Mul (LFPar ar (MulAdd (LFPar kr 0.2 0) 400 800) 0) 0.1)
+(Mul (LFPar ar 800 0) 0.1)
+(Mul (LFPar ar (XLine kr 100 8000 30 doNothing) 0) 0.1)
 
-(SinOsc ar (SinOsc kr (SinOsc kr 0.2 0 8 10) 0 400 800) 0 0.1)
-(SinOsc ar (SinOsc kr 0.2 0 400 800) 0 0.1)
-(SinOsc ar 800 0 0.1)
-(SinOsc ar (XLine kr 100 8000 30) 0 0.1)
+;; Compare:
 
-(LFTri ar (LFTri kr (LFTri kr 0.2 0 8 10) 0 400 800) 0 0.1)
-(LFTri ar (LFTri kr 0.2 0 400 800) 0 0.1)
-(LFTri ar 800 0 0.1)
-(LFTri ar (XLine kr 100 8000 30) 0 0.1)
+(Mul (SinOsc ar (MulAdd (SinOsc kr (MulAdd (SinOsc kr 0.2 0) 8 10) 0) 400 800) 0) 0.1)
+(Mul (SinOsc ar (MulAdd (SinOsc kr 0.2 0) 400 800) 0) 0.1)
+(Mul (SinOsc ar 800 0) 0.1)
+(Mul (SinOsc ar (XLine kr 100 8000 30 doNothing) 0) 0.1)
+
+;; Compare:
+
+(Mul (LFTri ar (MulAdd (LFTri kr (MulAdd (LFTri kr 0.2 0) 8 10) 0) 400 800) 0) 0.1)
+(Mul (LFTri ar (MulAdd (LFTri kr 0.2 0) 400 800) 0) 0.1)
+(Mul (LFTri ar 800 0) 0.1)
+(Mul (LFTri ar (XLine kr 100 8000 30 doNothing) 0) 0.1)
