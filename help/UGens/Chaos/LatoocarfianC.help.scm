badb95feb5570a4dc6df905e367a16557443dc01
Rohan Drape <rd@slavepianos.org>
2006-12-02 16:41:11 -0800

    Fixes to Latoocarfian[CLN] help files
    darcs-hash:126b09429a73dbd0185c37760234868dc907753f

--- help/UGens/Chaos/LatoocarfianC.help.scm
+++ help/UGens/Chaos/LatoocarfianC.help.scm
@@ -1,4 +1,6 @@
-;; (Latoocarfian a b c d)
+;; (LatoocarfianC a b c d xi yi)
+;; (LatoocarfianL a b c d xi yi)
+;; (LatoocarfianN a b c d xi yi)
 
 ;; This is a function given in Clifford Pickover's book Chaos In
 ;; Wonderland, pg 26.  The function has four parameters a, b, c, and
@@ -17,3 +19,19 @@
 ;; (silence) or oscillate in a cycle (tone).  This UGen is
 ;; experimental and not optimized currently, so is rather hoggish of
 ;; CPU.
+
+;; Default initial parameters.
+
+(Mul (LatoocarfianC ar (MouseX kr 20 (SampleRate ir) 0 0.1) 1 3 0.5 0.5 0.5 0.5) 0.2)
+
+;; Randomly modulate all parameters.
+
+(Mul (LatoocarfianC ar
+		    (FDiv (SampleRate ir) 4)
+		    (MulAdd (LFNoise2 kr 2) 1.5 1.5)
+		    (MulAdd (LFNoise2 kr 2) 1.5 1.5)
+		    (MulAdd (LFNoise2 kr 2) 0.5 1.5)
+		    (MulAdd (LFNoise2 kr 2) 0.5 1.5)
+		    0.5
+		    0.5)
+     0.2)
---
+++ help/UGens/Chaos/LatoocarfianN.help.scm
@@ -0,0 +1,5 @@
+;; (LatoocarfianC a b c d xi yi)
+;; (LatoocarfianL a b c d xi yi)
+;; (LatoocarfianN a b c d xi yi)
+
+;; See LatoocarfianC.
