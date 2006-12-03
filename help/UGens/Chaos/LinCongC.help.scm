77555a3b8c12339b7c9188edc96be5ea6e157995
Rohan Drape <rd@slavepianos.org>
2006-12-02 16:51:38 -0800

    Add help files for LinCong[CLN] UGens
    darcs-hash:a3557691e21512170605cecac9e85319de5c2a58

---
+++ help/UGens/Chaos/LinCongC.help.scm
@@ -0,0 +1,44 @@
+;; (LinCongC freq a c m xi)
+;; (LinCongL freq a c m xi)
+;; (LinCongN freq a c m xi)
+
+;; Linear congruential chaotic generator.
+
+;; freq - iteration frequency in Hertz
+;; a    - multiplier amount
+;; c    - increment amount
+;; m    - modulus amount
+;; xi   - initial value of x
+
+;; A cubic-interpolating sound generator based on the difference
+;; equation:
+
+;; 	xn+1 = (axn + c) % m
+
+;; The output signal is automatically scaled to a range of [-1, 1].
+
+
+;; Default initial parameters.
+
+(Mul (LinCongC ar (MouseX kr 20 (SampleRate ir) 0 0.1) 1.1 0.13 1 0) 0.2)
+
+;; Randomly modulate parameters.
+
+(Mul (LinCongC ar
+	       (MulAdd (LFNoise2 kr 1.0) 1e4 1e4)
+	       (MulAdd (LFNoise2 kr 0.1) 0.5 1.4)
+	       (MulAdd (LFNoise2 kr 0.1) 0.1 0.1)
+	       (LFNoise2 kr 0.1)
+	       0)
+     0.2)
+
+;; As frequency control...
+
+(Mul (SinOsc ar (MulAdd (LinCongC ar
+				  40 
+				  (MulAdd (LFNoise2 kr 0.1) 0.1 1.0)
+				  (MulAdd (LFNoise2 kr 0.1) 0.1 0.1)
+				  (LFNoise2 kr 0.1)
+				  0)
+			500 600) 0)
+     0.4)
---
+++ help/UGens/Chaos/LinCongN.help.scm
@@ -0,0 +1,5 @@
+;; (LinCongC freq a c m xi)
+;; (LinCongL freq a c m xi)
+;; (LinCongN freq a c m xi)
+
+;; See LinCongC.
