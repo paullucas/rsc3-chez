9a40fb3fc2a84833d69f1d9d77c32afff42b8e33
Rohan Drape <rd@slavepianos.org>
2006-12-08 04:20:21 -0800

    Fix definition of Convolution2
    darcs-hash:196292beee75684f76a315c20b91d67e2c554078

--- scheme/ugen/specialized.scm
+++ scheme/ugen/specialized.scm
@@ -12,7 +12,7 @@
 (define-specialized FFT (buf in) 1 kr)
 (define-specialized IFFT (buf) 1 ar)
 (define-specialized Convolution (in kernel frameSize) 1 ar)
-(define-specialized Convolution2 (in bufnum trigger frameSize) ar 1)
+(define-specialized Convolution2 (in bufnum trigger frameSize) 1 ar)
 
 (define-specialized PV_Add (bufA bufB) 1 kr)
 (define-specialized PV_BinScramble (buffer wipe width trig) 1 kr)
