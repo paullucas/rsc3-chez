da0ef633385e9ecefdaa366709b46aa38d491465
Rohan Drape <rd@slavepianos.org>
2006-12-19 23:38:31 -0800

    Add example at SampleDur help file
    darcs-hash:42235b8fcfe628cd687fcaa7251976cba444e91d

--- help/UGens/InfoUGens/SampleDur.help.scm
+++ help/UGens/InfoUGens/SampleDur.help.scm
@@ -2,3 +2,7 @@
 
 ;; Duration of one sample.  Equivalent to 1 / SampleRate.
 
+;; Compare a sine tone derived from sample rate with a 440Hz tone.
+
+(let ((freq (Mce (Mul (Recip SampleDur) 0.01) 440)))
+  (Mul (SinOsc ar freq 0) 0.1))
