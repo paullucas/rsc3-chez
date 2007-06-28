424f0227c6b3b043902ce2ab8e6404d3aab327a3
Rohan Drape <rd@slavepianos.org>
2007-06-27 23:44:50 -0700

    scheme/structure: write as plt module
    darcs-hash:77cba2c20fb7dd5bb81bcbf251d54e9f179203a3

--- scheme/structure/structure.scm
+++ scheme/structure/structure.scm
@@ -1,6 +1,11 @@
-;; structure.scm - (c) rohan drape, 2000-2006
+;; structure.scm - (c) rohan drape, 2000-2007
+
+(module structure mzscheme
+(provide (all-defined))
 
 (define-syntax define-structure
    (syntax-rules ()
      ((_ name field ...)
       (define-struct name (field ...) (make-inspector)))))
+
+)
