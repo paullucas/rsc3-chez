bcebc32baabd6fc8a4d4ed6c80a1cc0bc2cc5fae
Rohan Drape <rd@slavepianos.org>
2007-12-06 16:09:45 -0800

    Edits to Gate help file
    darcs-hash:ed4c6bf8a2a6f64e83fa5f209bb3b3654b667c8d

--- Help/UGens/Triggers/Gate.help.scm
+++ Help/UGens/Triggers/Gate.help.scm
@@ -2,5 +2,6 @@
 
 ;; The signal at `in' is passed while `trig' is greater than zero.
 
-(Gate (Mul (FSinOsc ar 500 0) 1/4)
-      (LFPulse ar 1 0 1/10))
+(let ((s (Mul (FSinOsc ar 500 0) 1/4))
+      (t (LFPulse ar 1 0 1/10)))
+  (audition (Out 0 (Gate s t))))
