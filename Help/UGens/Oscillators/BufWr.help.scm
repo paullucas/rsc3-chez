ece6b2cd3c5e765cd7bcb6cbca1b1299542fa903
Rohan Drape <rd@slavepianos.org>
2007-12-06 16:08:39 -0800

    Edits to BufWr help file
    darcs-hash:102b5f734af76ae8590e750bc745f1079899634d

--- Help/UGens/Oscillators/BufWr.help.scm
+++ Help/UGens/Oscillators/BufWr.help.scm
@@ -3,38 +3,35 @@
 ;; Buffer writing oscillator.  Write to a buffer at an index. See also
 ;; BufRd.
 
-;; bufnum		the index of the buffer to use
-;; phase   		modulateable index into the buffer (has to be audio rate).
-;; loop      		1 means true, 0 means false.  this is modulateable.
-;; input 		input ugens (channelArray)
+;; bufnum - the index of the buffer to use
+;; phase  - modulateable index into the buffer (has to be audio rate).
+;; loop   - 1 means true, 0 means false.  this is modulateable.
+;; input  - input ugens (channelArray)
 
-;; Note: BufWr (in difference to BufRd) does not do multichannel
-;; expansion, because input is an array.
+(let ((a (letc ((r 1))
+	   (let* ((r* (Mul (BufRateScale kr 0) r))
+		  (p (Phasor ar 0 r* 0 (BufFrames kr 0) 0))
+		  (f (MulAdd (LFNoise1 kr 2) 300 400))
+		  (i (Mul (SinOsc ar f 0) 0.1)))
+	     (Mrg (BufWr 0 p 1 i)
+		  (Out 0 0.0)))))
+      (b (letc ((r 1))
+	   (let* ((r* (Mul (BufRateScale kr 0) r))
+		  (p (Phasor ar 0 r* 0 (BufFrames kr 0) 0)))
+	     (Out 0 (BufRd 1 ar 0 p 1 2))))))
+  (with-sc3 
+   (lambda (fd)
+     (->< fd (/b_alloc 0 (* 44100 2) 1))
+     (send-synth fd "a" a)
+     (send-synth fd "b" b)
+     (-> s (/s_new "a" 1001 1 0))
+     (-> s (/s_new "b" 1002 1 0)))))
 
-(->< s (/b_alloc 0 (* 44100 2) 1))
+(define (send m)
+  (with-sc3 (lambda (fd) (-> fd m))))
 
-;; Write into the buffer with a BufWr
+(send (/n_set 1002 "r" 5))
 
-(send-synth
- s "a"
- (letc ((r 1))
-   (let ((p (Phasor ar 0 (Mul (BufRateScale kr 0) r) 0 (BufFrames kr 0) 0))
-	 (i (Mul (SinOsc ar (MulAdd (LFNoise1 kr 2) 300 400) 0) 0.1)))
-     (Mrg (BufWr 0 p 1 i)
-	  (Out 0 0.0)))))
+(send (/n_set 1001 "r" (rand 0 2)))
 
-(send-synth
- s "b"
- (letc ((r 1))
-   (let ((p (Phasor ar 0 (Mul (BufRateScale kr 0) r) 0 (BufFrames kr 0) 0)))
-     (Out 0 (BufRd 1 ar 0 p 1 2)))))
-
-(-> s (/s_new "a" 1001 1 0))
-
-(-> s (/s_new "b" 1002 1 0))
-
-(-> s (/n_set 1002 "r" 5))
-
-(-> s (/n_set 1001 "r" (rand 0 2)))
-
-(-> s (/n_set 1002 "r" 2))
+(send (/n_set 1002 "r" 2))
