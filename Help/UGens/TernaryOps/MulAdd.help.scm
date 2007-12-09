32ac1f14bf6338171359670138966a72ff22c882
Rohan Drape <rd@slavepianos.org>
2007-12-08 16:29:52 -0800

    Update TernaryOps help files
    (ie. use audition)
    
    darcs-hash:6b120aa847bb7233afceb4be6f36f8a187c2c6a3

--- Help/UGens/TernaryOps/MulAdd.help.scm
+++ Help/UGens/TernaryOps/MulAdd.help.scm
@@ -2,6 +2,8 @@
 
 ;; Functionally equivalent to (Add (Mul a b) c).
 
-(Mul (SinOsc ar (MulAdd (LFSaw kr (Mce 10 9) 0) 200 400) 0) 0.1)
+(let ((f (MulAdd (LFSaw kr (Mce 10 9) 0) 200 400)))
+  (audition (Out 0 (Mul (SinOsc ar f 0) 0.1))))
 
-(Mul (SinOsc ar (Add (Mul (LFSaw kr (Mce 10 9) 0) 200) 400) 0) 0.1)
+(let ((f (Add (Mul (LFSaw kr (Mce 10 9) 0) 200) 400)))
+  (audition (Out 0 (Mul (SinOsc ar f 0) 0.1))))
