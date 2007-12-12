7e50336a35bfd2865a13f315c04aa9e18e3bc3f9
Rohan Drape <rd@slavepianos.org>
2007-12-12 15:48:10 -0800

    Update DiskIO help files
    ie. use audition & with-sc3
    
    darcs-hash:5c541d8c3b1ded4d2c1c2cfc8d27cc4338b3d4eb

--- Help/UGens/DiskIO/DiskIn.help.scm
+++ Help/UGens/DiskIO/DiskIn.help.scm
@@ -9,13 +9,17 @@
 ;; looks like an input, but it is not an input, and cannot be set
 ;; using a control.
 
-(let ((f "/home/rohan/uc/uc-26/daily-practice/2006-10-04.wav")
-      (n 2))
-  (->< s (/b_alloc 0 8192 n))
-  (->< s (/b_read 0 f 0 -1 0 1))
-  (play s (DiskIn n ar 0)))
+(let ((f "/home/rohan/audio/metal.wav")
+      (n 1))
+  (with-sc3
+   (lambda (fd)
+     (->< fd (/b_alloc 0 8192 n))
+     (->< fd (/b_read 0 f 0 -1 0 1))
+     (play fd (Out 0 (DiskIn n ar 0))))))
 
-(reset s)
+(with-sc3 reset)
 
-(begin (->< s (/b_close 0))
-       (->< s (/b_free 0)))
+(with-sc3
+ (lambda (fd)
+   (->< fd (/b_close 0))
+   (->< fd (/b_free 0))))
--- Help/UGens/DiskIO/DiskOut.help.scm
+++ Help/UGens/DiskIO/DiskOut.help.scm
@@ -7,42 +7,34 @@
 
 (let ((bus-size 1024)
       (bufferexpt 15))
-  (= 0 (bitwise-and (expt 2 bufferexpt) 
+  (= 0 (bitwise-and (expt 2 bufferexpt)
 		    (- (arithmetic-shift bus-size 1) 1))))
 
-(->< 
- s
- (/d_recv 
-  (graphdef->u8l
-   (synthdef
-    "DiskOut-Help"
-    (letc ((bufnum 0))
-      (let ((z (Clip2 
-		(RLPF (LFPulse ar (MulAdd (SinOsc kr 0.2 0) 10 21) 
-			       (Mce 0 0.1) 0.1) 100 0.1) 0.4)))
-	(Mrg (DiskOut bufnum z)
-	     (Out 0 z))))))))
-
-(define b 10)
-
-(->< s (/b_alloc b 32768 2))
-
-(->< s (/b_write b 
-		 "/tmp/test.aiff" 
-		 "aiff"
-		 "float"
-		 32768
-		 0
-		 1))
-
-(define n 100)
-
-(-> s (/s_new "DiskOut-Help" n 1 1 "bufnum" b))
-
-(-> s (/n_free n))
-
-(->< s (/b_close b))
-
-(->< s (/b_free b))
+(let ((b 10)
+      (n 100)
+      (t 10)
+      (g (letc ((bufnum 0))
+	   (let ((z (Clip2
+		     (RLPF (LFPulse ar (MulAdd (SinOsc kr 0.2 0) 10 21)
+				    (Mce 0 0.1) 0.1) 100 0.1) 0.4)))
+	     (Mrg (DiskOut bufnum z)
+		  (Out 0 z))))))
+  (with-sc3
+   (lambda (fd)
+     (->< fd (/d_recv (graphdef->u8l (synthdef "DiskOut-Help" g))))
+     (->< fd (/b_alloc b 32768 2))
+     (->< fd (/b_write b
+		       "/tmp/test.aiff"
+		       "aiff"
+		       "float"
+		       32768
+		       0
+		       1))
+     (-> fd (/s_new "DiskOut-Help" n 1 1 "bufnum" b))
+     (sleep t)
+     (-> fd (/n_free n))
+     (->< fd (/b_close b))
+     (->< fd (/b_free b)))))
 
 (system "sndfile-info /tmp/test.aiff")
+(system "jack.play /tmp/test.aiff")
