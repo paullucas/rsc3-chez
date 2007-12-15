a167afd4d948de3a3a5a2161b5e6b73dae5a37e8
Rohan Drape <rd@slavepianos.org>
2007-12-14 17:25:58 -0800

    Edit pvcollect help file
    darcs-hash:6ff6d69868adfb8f69c401ed433c7d12868ae8c4

--- Help/UGens/FFT/pvcollect.help.scm
+++ Help/UGens/FFT/pvcollect.help.scm
@@ -1,3 +1,24 @@
+;; (pvcollect chain numframes func frombin tobin zeroothers)
+
+;; Process each bin of an FFT chain separately.
+
+;; pvcollect applies function func to each bin of an FFT chain. func
+;; should be a function that takes magnitude, phase, index as inputs
+;; and returns a resulting [magnitude, phase].
+
+;; The "index" is the integer bin number, starting at 0 for DC. You
+;; can optionally ignore the phase and only return a single
+;; (magnitude) value, in which case the phase is assumed to be left
+;; unchanged.
+
+;; frombin, tobin, and zeroothers are optional arguments which limit
+;; the processing to a specified integer range of FFT bins. If
+;; zeroothers is set to 1 then bins outside of the range being
+;; processed are silenced.
+
+;; Note that this procedure can be relatively CPU-heavy, depending on
+;; how you use it.
+
 (require srfi/1)
 
 (define no-op
@@ -20,9 +41,10 @@
   (lambda (m p _)
     (list (Mul (GT (LFNoise0 kr 10) 0) m) p)))
 
-(define (spectral-delay v)
+(define spectral-delay
   (lambda (m p _)
-    (list (Add m (DelayN m 1 v)) p)))
+    (let ((v (range (LFPar kr 0.5 0) 0.1 1)))
+      (list (Add m (DelayN m 1 v)) p))))
 
 (define (bpf-sweep nf)
   (lambda (m p i)
@@ -34,10 +56,8 @@
    (->< fd (/b_alloc 10 1024 1))
    (->< fd (/b_allocRead 11 "/home/rohan/audio/metal.wav" 0 0))))
 
-(let* ((f bpf-sweep)
-       (nf 1024)
+(let* ((nf 1024)
        (i (PlayBuf 1 11 (BufRateScale kr 11) 1 0 1))
        (c1 (FFT* 10 i))
-       (v (range (LFPar kr 0.5 0) 0.1 1))
-       (c2 (pvcollect c1 nf (spectral-delay v) 0 250 0)))
+       (c2 (pvcollect c1 nf spectral-delay 0 250 0)))
   (audition (Out 0 (Mul 0.1 (IFFT* c2)))))
