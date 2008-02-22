737cc8aba25ba5d396895f3222a14ffd3e686cd4
Rohan Drape <rd@slavepianos.org>
2008-02-21 18:10:15 -0800

    Edit example at DegreeToKey help file
    darcs-hash:d61b27def013639491e425a439a10a93b288db1e

--- Help/UGens/Controls/DegreeToKey.help.scm
+++ Help/UGens/Controls/DegreeToKey.help.scm
@@ -13,18 +13,20 @@
 
 ;; octave - the number of steps per octave in the scale.
 
-(->< s (/b_alloc 0 7 1))
-
-(-> s (/b_setn* 0 0 (list 0 2 3.2 5 7 9 10)))
-
-(let* ((k (DegreeToKey 0 (MouseX kr 0 15 0 0.1) 12))
-       (n (Mul (LFNoise1 kr (Mce 3 3.05)) 0.04))
-       (f (lambda (b)
-	    (let* ((o (Mul (SinOsc ar (MIDICPS (Add* b k n)) 0) 0.1))
-		   (d (Mul (RLPF (LFPulse ar (MIDICPS (Mce 48 55)) 0.15 0.5)
-				 (MIDICPS (MulAdd (SinOsc kr 0.1 0) 10 b))
-				 0.1)
-			   0.1))
+(let* ((b 0)
+       (p (list 0 2 3.2 5 7 9 10))
+       (x (MouseX kr 0 15 0 0.1))
+       (k (DegreeToKey 0 x 12))
+       (c (lambda (n r)
+	    (let* ((o (Mul (SinOsc ar (MIDICPS (Add* r k n)) 0) 0.1))
+		   (t (LFPulse ar (MIDICPS (Mce 48 55)) 0.15 0.5))
+		   (f (MIDICPS (MulAdd (SinOsc kr 0.1 0) 10 r)))
+		   (d (Mul (RLPF t f 0.1) 0.1))
 		   (m (Add o d)))
 	      (Add (CombN m 0.31 0.31 2) m)))))
-  (Mul (Add (f 48) (f 72)) 0.25))
+  (with-sc3
+   (lambda (fd)
+     (->< fd (/b_alloc 0 7 1))
+     (-> fd (/b_setn* 0 0 p))
+     (let ((n (Mul (LFNoise1 kr (Mce 3 3.05)) 0.04)))
+       (play fd (Out 0 (Mul (Add (c n 48) (c n 72)) 0.25)))))))
