ab6c3849c61397b93494575cf36bb9de6d0a8de8
Rohan Drape <rd@slavepianos.org>
2008-03-11 17:46:24 -0700

    iota/enum-from-to edits
    darcs-hash:a9ab12e2ca3e8864a5177f3f3eda28db101f46ab

--- supercollider.scm
+++ supercollider.scm
@@ -145,18 +145,15 @@
 (define (packfft-data* mp)
   (make-mce (cons (* 2 (length mp)) (splice mp))))
 
-(define (from-to f t)
-  (iota (+ 1 (- t f)) f))
-
 (define (unpack-fft c nf from to mp?)
   (map (lambda (i) 
 	 (Unpack1FFT c nf i mp?))
-       (from-to from to)))
+       (enum-from-to from to)))
 
 (define (pvcollect c nf f from to z?)
   (let* ((m (unpack-fft c nf from to 0))
 	 (p (unpack-fft c nf from to 1))
-	 (i (from-to from to))
+	 (i (enum-from-to from to))
 	 (e (map f m p i)))
     (PackFFT c nf from to z? (packfft-data* e))))
 
