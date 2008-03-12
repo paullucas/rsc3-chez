c36de6f9bd67e6e545192e7251652bd952282fcd
Rohan Drape <rd@slavepianos.org>
2008-03-11 19:58:39 -0700

    clipu, clipz (delete)
    darcs-hash:a63ba2d8a8a16d1daabe445b5b6153b928d6edb7

--- common.scm
+++ common.scm
@@ -86,21 +86,12 @@
       (f t nil))))
 
 
-;; clip
+;; math
 
 ;; Clip `n' between a and b.
-
-(define (clip a b n) (if (< n a) a (if (> n b) b n)))
-(define (clipu n)    (clip 0 1 n))
-
-;; Clip real and imaginary components of z.
-
-(define (clipz a b z)
-  (make-rectangular (clip a b (real-part z))
-		    (clip a b (imag-part z))))
-
-
-;; common
+(define clip
+  (lambda (a b n) 
+    (if (< n a) a (if (> n b) b n))))
 
 (define (squared n)
   (* n n))
