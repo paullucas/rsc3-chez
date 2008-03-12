67399988b247618ab05f2065084d89ded14eee87
Rohan Drape <rd@slavepianos.org>
2008-03-11 21:30:53 -0700

    construct-ugen (edit)
    darcs-hash:3afa376e0c16ce14f38d18d8ff165597831511c1

--- ugen.scm
+++ ugen.scm
@@ -11,23 +11,22 @@
 ;; It is significant whether MCE or Proxing occurs first, the ordering
 ;; here follows that in sclang.
 
-(define (construct-ugen name rate? inputs mce? outputs special id)
-  (let* ((inputs* (if mce?
-		      (append inputs (mce-l mce?))
-		      inputs))
-	 (rate (if rate?
-		   rate?
-		   (rate-select (map rate-of inputs*))))
-	 (u (make-ugen
-	     (if (symbol? name)
-		 (symbol->string name)
-		 name)
-	     rate
-	     inputs*
-	     (make-outputs outputs rate)
-	     special
-	     id)))
-    (proxied (mced u))))
+(define construct-ugen
+  (lambda (name rate? inputs mce? outputs special id)
+    (let* ((inputs* (if mce?
+			(append2 inputs (mce-l mce?))
+			inputs))
+	   (rate (if rate?
+		     rate?
+		     (rate-select (map rate-of inputs*))))
+	   (u (make-ugen
+	       (symbol->string name)
+	       rate
+	       inputs*
+	       (make-outputs outputs rate)
+	       special
+	       id)))
+      (proxied (mced u)))))
 
 
 ;; graph
