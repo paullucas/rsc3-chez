74dd371c432b3b8c13834300ad9449d0d5a31768
Rohan Drape <rd@slavepianos.org>
2008-03-13 19:27:17 -0700

    Binary comparators (edits)
    darcs-hash:97393cda6a73af6eae4dd3dd40463ad535b62d29

--- Help/UGens/BinaryOps/LE.help.scm
+++ Help/UGens/BinaryOps/LE.help.scm
@@ -1,12 +1,3 @@
 ;; (LE a b)
 
-;; Less than or equal to, written '<=' in sclang.  
-
-;; Signal is 1.0 if a <= b, otherwise it is 0.0. This can be useful
-;; for triggering purposes, among other things.
-
-;; Trigger an envelope.
-
-(let ((t (LE (SinOsc ar 1 0) 0)))
-  (Mul (SinOsc ar 440 0)
-       (EnvGen kr t 0.1 0 1 doNothing (env/perc 0.01 1 1 -4))))
+;; See GT
--- Help/UGens/Envelopes/EnvGen.help.scm
+++ Help/UGens/Envelopes/EnvGen.help.scm
@@ -30,13 +30,13 @@
 
 ;; Percussive envelope
 
-(let* ((d (env/perc 0.01 1 1 -4))
+(let* ((d (env/perc 0.01 1 1 (list -4 -4)))
        (e (EnvGen kr 1 0.1 0 1 removeSynth d)))
   (audition (Out 0 (Mul e (SinOsc ar 440 0)))))
 
 ;; The break-point assistant makes a static envelope from a
 ;; co-ordinate list.  There is a duration and amplitude scalar.
 
-(let* ((d (env/bp '(0 0 1/2 1 1 0) 1 1))
+(let* ((d (env/bp (list 0 0 1/2 1 1 0) 1 1 (replicate 3 "linear")))
        (e (EnvGen kr 1 0.1 0 1 removeSynth d)))
   (audition (Out 0 (Mul e (SinOsc ar 440 0)))))
