04919f93c3267c71ae0bc00e25b8013d4d24396a
Rohan Drape <rd@slavepianos.org>
2008-03-20 17:24:11 -0700

    Add noise-burst-sweep graph
    darcs-hash:21d993622bea7b3bcf955b41bcaccc36cf23853c

--- help/graph/noise-burst-sweep.scm
+++ help/graph/noise-burst-sweep.scm
@@ -1,1 +1,8 @@
 ;; noise burst sweep (jmcc)
+
+(let* ((n (clone 2 (white-noise ar)))
+       (lfo-rate (mouse-x kr 10 60 1 0.2))
+       (amp (u:max 0 (lfsaw kr lfo-rate -1)))
+       (cfreq (mouse-y kr 400 8000 1 0.2))
+       (freq (mul-add (sin-osc kr 0.2 0) cfreq (mul 1.05 cfreq))))
+  (audition (out 0 (resonz (mul n amp) freq 0.1))))
