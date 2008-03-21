c6dec50c7261f3c9f9b72ddda7893a6859d0eec0
Rohan Drape <rd@slavepianos.org>
2008-03-21 01:25:38 -0700

    Fix haskellisms
    darcs-hash:eefaad9456b2b5623768690dc9ba0c0715037a89

--- help/graph/one-line.scm
+++ help/graph/one-line.scm
@@ -1,6 +1,6 @@
 ;; one-line (lance putnam)
 
 (let* ((lfs (mul-add (lf-saw ar (mce2 1 0.99) (mce2 0 0.6)) 2000 2000))
-       (lfs_t (mul (trunc lfs (mce2 400 600)) (mce2 1 -1)))
-       (f (one-pole (mix lfs_t) 0.98)))
+       (lfs-t (mul (trunc lfs (mce2 400 600)) (mce2 1 -1)))
+       (f (one-pole (mix lfs-t) 0.98)))
   (audition (out 0 (pan2 (sin-osc ar f 0) 0 0.1))))
