31abaf710bf1418aa0d9b90832ad56234e74c445
Rohan Drape <rd@slavepianos.org>
2008-03-23 16:08:05 -0700

    Modify util/mk to write r6rs/rsc3.ikarus.sls
    darcs-hash:bf79bc52c51d9efaed6cfcf254a2ec8092e10619

--- util/mk.scm
+++ util/mk.scm
@@ -1,13 +1,13 @@
 (import (rhs r6rs rhs))
 (load "/home/rohan/sw/rhs/util/util.scm")
 
-(mk-r6rs '(rsc3 ikarus rsc3)
+(mk-r6rs '(rsc3 r6rs rsc3)
 	 '("/home/rohan/sw/rsc3/src/rsc3.scm")
-	 "/home/rohan/sw/rsc3/ikarus/rsc3.scm"
+	 "/home/rohan/sw/rsc3/r6rs/rsc3.ikarus.sls"
 	 '((rnrs)
 	   (rhs r6rs rhs)
 	   (sosc r6rs sosc)
-	   (sosc ikarus transport)
+	   (sosc r6rs transport)
 	   (only (ikarus) parameterize tcp-connect)
 	   (xitomatl srfi random))
 	 '()
