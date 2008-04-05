5919094c8af620ffa1bba84a00b0c91a00b718de
Rohan Drape <rd@slavepianos.org>
2008-04-05 04:33:42 -0700

    adso graph (clarity edits)
    darcs-hash:6d3a9b4641713c31814bf49436640d3f7b89d2d4

--- help/graph/adso.scm
+++ help/graph/adso.scm
@@ -1,35 +1,37 @@
 ;; adso (rd)
 
-(letrec ((n 24)
-         (b-get
-          (lambda (b n)
-            (buf-rd 1 kr b (make-mce (enum-from-to 0 (- n 1))) 0 0)))
-         (rand-l
-          (lambda (n l r)
-            (replicate-m n (random l r))))
-         (pattern
-          (lambda (fd)
-            (let ((z (map floor
-                          (replicate-m n (random (random 22 48)
-                                                 (random 54 122))))))
-              (send fd (b-setn1 0 0 z))
-              (send fd (b-setn1 1 0 (rand-l n 0 .1)))
-              (send fd (b-setn1 2 0 (rand-l n -1 1)))
-              (send fd (b-setn1 3 0 (rand-l n 2 12)))
-              (send fd (b-setn1 4 0 (rand-l n 0.001 0.0075)))
-              (send fd (b-setn1 5 0 (rand-l n 1 24)))
-              (send fd (b-setn1 6 0 (rand-l n 0.05 (random 0.075 2.4))))
-              (thread-sleep (choose (list 0.025 0.05 0.075 0.1 0.125 2.5)))
-              (pattern fd))))
-         (adso
-          (out 0 (mix (pan2 (sin-osc ar
-                                     (mul (midi-cps (b-get 0 n))
-                                          (mul-add (sin-osc kr (b-get 3 n) 0)
-                                                   (b-get 4 n)
-                                                   1))
-                                     0)
+(let* ((n 24)
+       (rand-l
+        (lambda (n l r)
+          (replicate-m n (random l r))))
+       (adso
+        (let* ((b-get
+                (lambda (b n)
+                  (let ((j (make-mce (enum-from-to 0 (- n 1)))))
+                    (buf-rd-n 1 kr b j 0))))
+               (m (sin-osc kr (b-get 3 n) 0))
+               (f (mul (midi-cps (b-get 0 n))
+                       (mul-add m (b-get 4 n) 1))))
+          (out 0 (mix (pan2 (sin-osc ar f 0)
                             (b-get 2 n)
-                            (mul (b-get 1 n) 1))))))
+                            (b-get 1 n))))))
+       (pattern
+        (lambda (fd)
+          (lambda (t)
+            (let ((z (map floor
+                          (replicate-m
+                           n (random (random 22 48)
+                                     (random 54 122)))))
+                  (rn (lambda (i j k)
+                        (send fd (b-setn1 i 0 (rand-l n j k))))))
+              (send fd (b-setn1 0 0 z))
+              (rn 1 0 0.1)
+              (rn 2 -1 1)
+              (rn 3 2 12)
+              (rn 4 0.001 0.0075)
+              (rn 5 1 24)
+              (rn 6 0.05 (random 0.075 2.4))
+              (thread-sleep t))))))
   (with-sc3
    (lambda (fd)
      (for-each
@@ -37,4 +39,9 @@
         (async fd (b-alloc i n 1)))
       (enum-from-to 0 6))
      (play fd adso)
-     (pattern fd))))
+     (map (pattern fd)
+          (replicate-m
+           32
+           (choose
+            (list 0.025 0.05 0.075 0.1 0.125 2.5))))
+     (reset fd))))
