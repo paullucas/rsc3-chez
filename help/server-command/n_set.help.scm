c83522c2a50f9efc3ba2672c5136b991ad1953dd
Rohan Drape <rd@slavepianos.org>
2008-04-11 01:19:58 -0700

    add examples to n_set help file
    darcs-hash:9cb28173cf2c98d0d507c91820360eb989da9b2e

--- help/server-command/n_set.help.scm
+++ help/server-command/n_set.help.scm
@@ -10,3 +10,18 @@
 ; controls to those values. If the node is a group, then it sets the
 ; controls of every node in the group.
 
+(with-sc3
+ (lambda (fd)
+   (letc ((f 440)
+          (a 0.1))
+     (send-synth fd "sin" (out 0 (mul (sin-osc ar f 0) a))))
+   (send fd (s-new0 "sin" 1001 add-to-tail 1))))
+
+(with-sc3
+ (lambda (fd)
+   (send fd (n-set1 1001 "f" 1280))))
+
+(with-sc3
+ (lambda (fd)
+   (send fd (n-set 1001 (list (tuple2 "f" (random 60 900))
+                              (tuple2 "a" (random 0.05 0.25)))))))
