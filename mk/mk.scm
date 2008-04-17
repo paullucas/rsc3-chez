f00c19a455ff7e6ab5f4eeff99709832d15eabd4
Rohan Drape <rd@slavepianos.org>
2008-04-16 22:27:18 -0700

    update build system
    darcs-hash:95e695aae6f3a49886eb8c4f5b874d7868f94b10

--- mk/mk.scm
+++ mk/mk.scm
@@ -1,14 +1,14 @@
 (import (rnrs)
-        (rhs util util))
+        (mk-r6rs))
 
-(mk-r6rs '(rsc3 r6rs rsc3)
+(mk-r6rs '(rsc3)
 	 '("/home/rohan/sw/rsc3/src/rsc3.scm"
 	   "/home/rohan/sw/rsc3/src/time.ikarus.scm")
-	 "/home/rohan/sw/rsc3/r6rs/rsc3.ikarus.sls"
+	 (string-append (list-ref (command-line) 1)
+                        "/rsc3.ikarus.sls")
 	 '((rnrs)
-	   (rhs r6rs rhs)
-	   (sosc r6rs sosc)
-	   (sosc r6rs transport)
+	   (rhs)
+	   (sosc)
 	   (prefix (ikarus) ikarus:)
 	   (prefix (xitomatl srfi random) srfi:)
            (prefix (xitomatl srfi time) srfi:))
