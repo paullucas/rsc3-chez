3104be0902d0e47e57a9390169868f6e2a636822
Rohan Drape <rd@slavepianos.org>
2008-04-16 21:34:03 -0700

    track edits to ikarus:nanosleep
    darcs-hash:b388dd3fd20a38db1aafb1309b2dca9d0be9dcbb

--- src/time.ikarus.scm
+++ src/time.ikarus.scm
@@ -1,7 +1,7 @@
-;; double -> bool
+;; double -> void
 (define thread-sleep
   (lambda (p)
     (let* ((s (exact (floor p)))
 	   (f (- p s))
 	   (ns (exact (round (* f 1000000000)))))
-      (= 0 (ikarus:nanosleep s ns)))))
+      (ikarus:nanosleep s ns))))
