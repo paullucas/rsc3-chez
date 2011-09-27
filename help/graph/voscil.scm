d6527d9aa5855171329845c4b5665a5f90d3736e
Rohan Drape <rd@slavepianos.org>
2011-09-27 02:55:55 -0700

    help/graph: r6rs-script (remainder)
    Ignore-this: 8e0c803051cb64f2845f1a8eaac2f71b
    
    darcs-hash:609bef2f233062ffaaa8ab3fb3f019923be8b37b

--- help/graph/voscil.scm
+++ help/graph/voscil.scm
@@ -1,21 +1,26 @@
 ;; voscil (rd)
 
-(let* ((b 32)
-       (hb (/ (- b 1) 2))
-       (n (* 8192 4))
-       (r 6)
-       (f 600)
-       (s (add
-           (pan2 (add (v-osc ar (mul-add (lf-noise0 kr r) hb hb) (* f 2) 0)
-                      (mul (blip ar
-                                 (mul-add (lf-noise0 kr r) 40 600)
-                                 (mul-add (lf-noise0 kr r) 16 24))
-                           (mul-add (lf-noise0 kr r) 0.1 0.1)))
-                 (lf-noise0 kr r)
-                 (mul-add (lf-noise0 kr r) 0.5 0.5))
-           (pan2 (v-osc ar (mul-add (lf-saw kr (/ 1 r) 0) hb hb) f 0)
-                 (lf-noise0 kr r)
-                 (mul-add (lf-noise0 kr r) 0.5 0.5)))))
+(import (rnrs) (rhs) (sosc) (rsc3))
+
+(define voscil
+  (lambda (b)
+    (let* ((hb (/ (- b 1) 2))
+           (r 6)
+           (f 600))
+      (add
+       (pan2 (add (v-osc ar (mul-add (lf-noise0 kr r) hb hb) (* f 2) 0)
+                  (mul (blip ar
+                             (mul-add (lf-noise0 kr r) 40 600)
+                             (mul-add (lf-noise0 kr r) 16 24))
+                       (mul-add (lf-noise0 kr r) 0.1 0.1)))
+             (lf-noise0 kr r)
+             (mul-add (lf-noise0 kr r) 0.5 0.5))
+       (pan2 (v-osc ar (mul-add (lf-saw kr (/ 1 r) 0) hb hb) f 0)
+             (lf-noise0 kr r)
+             (mul-add (lf-noise0 kr r) 0.5 0.5))))))
+
+(let ((n (* 8192 4))
+      (b 32))
   (with-sc3
    (lambda (fd)
      (for-each
@@ -25,4 +30,4 @@
          (i-random 2 512)
          (send fd (b-set1 i (i-random 0 n) (random -1 1)))))
       (enum-from-to 0 (- b 1)))
-     (play fd (out 0 s)))))
+     (play fd (out 0 (voscil b))))))
