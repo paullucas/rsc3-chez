d43e291ee487fedc8c73cf8863d59bd464d4b691
Rohan Drape <rd@slavepianos.org>
2014-06-21 19:57:11 -0700

    +src/external.scm
    Ignore-this: 33207215480255f716560512fcaf5925
    
    darcs-hash:3f362b924792d267e443eda5e3826e0893bd7137

---
+++ src/external.scm
@@ -0,0 +1,4 @@
+(define dust-r (mk-oscillator-id "DustR" (lo hi) 1))
+(define exp-rand-n (mk-specialized-n-id "ExpRandN" (l r) ir))
+(define rand-n (mk-specialized-n-id "RandN" (l r) ir))
+(define t-scramble (mk-oscillator-mce "TScramble" (tr i) 1))
