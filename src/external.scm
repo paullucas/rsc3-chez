346eb11868bac7b62235fce3eccccc83da7a30fe
Rohan Drape <rd@slavepianos.org>
2014-06-24 01:05:25 -0700

    +i-rand-n,help/graph/seqr-n"
    Ignore-this: 386afa155f363b75662c95402c039805
    
    darcs-hash:dcb2831ca2b8ef4b1569ab89d62f08c99d4d7422

--- src/external.scm
+++ src/external.scm
@@ -1,5 +1,6 @@
 (define dust-r (mk-oscillator-id "DustR" (lo hi) 1))
 (define exp-rand-n (mk-specialized-n-id "ExpRandN" (l r) ir))
+(define i-rand-n (mk-specialized-n-id "IRandN" (l r) ir))
 (define lin-rand-n (mk-specialized-n-id "LinRandN" (l r bias) ir))
 (define rand-n (mk-specialized-n-id "RandN" (l r) ir))
 (define t-scramble (mk-oscillator-mce "TScramble" (tr i) 1))
