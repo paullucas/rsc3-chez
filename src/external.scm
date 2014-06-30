310e39bc8eeacf196962553bf1e03ba2cc40baa9
Rohan Drape <rd@slavepianos.org>
2014-06-29 17:29:09 -0700

    +r-delay-map
    Ignore-this: 773d08af1f3e67f528988bb01a3c0e6f
    
    darcs-hash:1c397de5b945a76dfdb62ea89a36ca4cf1671207

--- src/external.scm
+++ src/external.scm
@@ -3,4 +3,5 @@
 (define i-rand-n (mk-specialized-n-id "IRandN" (l r) ir))
 (define lin-rand-n (mk-specialized-n-id "LinRandN" (l r bias) ir))
 (define rand-n (mk-specialized-n-id "RandN" (l r) ir))
+(define r-delay-map (mk-filter-mce "RDelayMap" (buf in dyn spec-array) 1))
 (define t-scramble (mk-oscillator-mce "TScramble" (tr i) 1))
