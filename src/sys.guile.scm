430e7ab2083f086b363218c82f67771b96da2a80
Rohan Drape <rd@slavepianos.org>
2014-08-07 00:45:30 -0700

    +src/sys.guile
    Ignore-this: 47832b3c070991566271edcafd6c784c
    
    darcs-hash:0aaa0b3837c0b688f1ae744084adc31977dabe0f

---
+++ src/sys.guile.scm
@@ -0,0 +1,13 @@
+;; () -> double
+(define utcr
+  (lambda ()
+    (let* ((t (gettimeofday))
+           (s (car t))
+           (u (cdr t)))
+      (+ s (/ u 1e6)))))
+
+;; double -> void
+(define thread-sleep
+  (lambda (p)
+    (let ((u (inexact->exact (round (* p 1e6)))))
+      (usleep u))))
